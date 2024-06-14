c54ba46a1ef5db7221210d57104c7b0991ef2243
Tempormods <louis2772722727@gmail.com>
2024-06-13 21:09:44 -0400

    new files added

--- src/main/java/net/tempormods/endless_ores/EndlessOreMod.java
+++ src/main/java/net/tempormods/endless_ores/EndlessOreMod.java
@@ -1,6 +1,8 @@
 package net.tempormods.endless_ores;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -13,6 +15,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.tempormods.endless_ores.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -28,6 +31,8 @@ public class EndlessOreMod
     public EndlessOreMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
@@ -50,7 +55,9 @@ public class EndlessOreMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.DEEP_CRYSTAL);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
--- src/main/java/net/tempormods/endless_ores/item/ModItems.java
+++ src/main/java/net/tempormods/endless_ores/item/ModItems.java
@@ -1,4 +1,20 @@
 package net.tempormods.endless_ores.item;
 
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.tempormods.endless_ores.EndlessOreMod;
+
 public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, EndlessOreMod.MOD_ID);
+
+    public static final RegistryObject<Item> DEEP_CRYSTAL = ITEMS.register("deep_crystal",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
